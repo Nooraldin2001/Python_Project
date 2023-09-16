f730996067152662a4adacda0fcc13de7a712d09
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-09-16 19:45:52 +0300

    Task 1.2 solved

--- Tasks.py
+++ Tasks.py
@@ -10,6 +10,14 @@ def list_odd_even(x, y):
 print(list_odd_even(5,50))         
  
 
+'''
+(Task 1.2) Create a python function that takes 2 numbers x,y and prints all the numbers between 1 and 100 than can
+be divided on x,y
+'''
+def list_Numbers(x, y):
+    number_list = [num for num in range(1,101) if num % x == 0 or num % y == 0]   
+    return 'This is the numbers that can be divided on x,y: ',number_list
 
+print(list_Numbers(5,10))     
          
 
